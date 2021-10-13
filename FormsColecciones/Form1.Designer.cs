7e81439b29e08b9d64dc814f48358d0478083d59
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-10-13 14:45:47 -0500

    Formulario

---
+++ FormsColecciones/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace FormsColecciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtbNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbTelfono = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbEdad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbDireccion = new System.Windows.Forms.TextBox();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(165, 456);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(202, 62);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtbNombre
+            // 
+            this.txtbNombre.Location = new System.Drawing.Point(258, 43);
+            this.txtbNombre.Name = "txtbNombre";
+            this.txtbNombre.Size = new System.Drawing.Size(403, 38);
+            this.txtbNombre.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 32);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Teléfono";
+            // 
+            // txtbTelfono
+            // 
+            this.txtbTelfono.Location = new System.Drawing.Point(258, 139);
+            this.txtbTelfono.Name = "txtbTelfono";
+            this.txtbTelfono.Size = new System.Drawing.Size(403, 38);
+            this.txtbTelfono.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 32);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Edad";
+            // 
+            // txtbEdad
+            // 
+            this.txtbEdad.Location = new System.Drawing.Point(258, 232);
+            this.txtbEdad.Name = "txtbEdad";
+            this.txtbEdad.Size = new System.Drawing.Size(403, 38);
+            this.txtbEdad.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 32);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Dirección";
+            // 
+            // txtbDireccion
+            // 
+            this.txtbDireccion.Location = new System.Drawing.Point(258, 307);
+            this.txtbDireccion.Name = "txtbDireccion";
+            this.txtbDireccion.Size = new System.Drawing.Size(403, 38);
+            this.txtbDireccion.TabIndex = 2;
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(560, 467);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(202, 62);
+            this.btnSiguiente.TabIndex = 1;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 635);
+            this.Controls.Add(this.txtbDireccion);
+            this.Controls.Add(this.txtbEdad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbTelfono);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtbNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbTelfono;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbEdad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbDireccion;
+        private System.Windows.Forms.Button btnSiguiente;
+    }
+}
+
---
+++ FormsColecciones/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace FormsColecciones
+{
+    public partial class Form1 : Form
+    {
+        ArrayList clientes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            clientes = new ArrayList();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            clientes.Add(new Cliente(txtbNombre.Text, txtbTelfono.Text, txtbEdad.Text, txtbDireccion.Text));
+            txtbNombre.Clear();
+            txtbTelfono.Clear();
+            txtbEdad.Clear();
+            txtbDireccion.Clear();
+        }
+    }
+}
