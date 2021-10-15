f42bcab937e96e695f79d4d7a1f3e91e986abf21
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-10-15 12:28:47 -0500

    Validando Compos

--- FormsColecciones/Form1.cs
+++ FormsColecciones/Form1.cs
@@ -44,12 +44,31 @@ namespace FormsColecciones
 
         private void btnSiguiente_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Cliente cliente = (Cliente)clientes[Indice++];
 
-            Cliente cliente = (Cliente)clientes[Indice++];
-            txtbNombre.Text = cliente.Nombre;
-            txtbTelfono.Text = cliente.Telefono;
-            txtbEdad.Text = cliente.Edad;
-            txtbDireccion.Text = cliente.Direccion ;
+                if( cliente.Nombre == ""  )
+                {
+                    throw new ApplicationException("Se debe agregar un cliente");
+                }
+
+
+                txtbNombre.Text = cliente.Nombre;
+                txtbTelfono.Text = cliente.Telefono;
+                txtbEdad.Text = cliente.Edad;
+                txtbDireccion.Text = cliente.Direccion;
+            }            
+            catch(ArgumentOutOfRangeException  error )
+            {
+                MessageBox.Show("No hay ningún cliente agregado \n" + error.Message);
+            }
+            catch(ApplicationException error   )
+            {
+                MessageBox.Show("No se deben almacenar campos vacíos\n" + error.Message );
+                clientes.RemoveAt(Indice--);
+            }
+
         }
 
         private void btnAnterior_Click(object sender, EventArgs e)
